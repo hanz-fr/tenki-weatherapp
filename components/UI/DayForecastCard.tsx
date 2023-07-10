7ecb4478fbd92e0c6049ec7e41dd9a31efc57722
Han <59479453+recta-rrae@users.noreply.github.com>
2023-07-10 22:40:41 +0700

    (layout) : added flex basis to DayForecastCard

--- components/UI/DayForecastCard.tsx
+++ components/UI/DayForecastCard.tsx
@@ -45,8 +45,8 @@ export default function DayForecastCard(props: ForecastProps) {
   return (
     <>
       <div className="border-t-[0.6px] border-[#30373E] my-8"></div>
-      <div className="flex flex-row px-2 xl:px-5">
-        <div className="basis-1/3 xl:basis-1/6 flex flex-col">
+      <div className="flex flex-row xl:px-5">
+        <div className="basis-1/3 md:basis-1/4 xl:basis-1/6 flex flex-col">
           <Image
             src="/assets/svg/weather_sunny.svg"
             width={30}
@@ -56,7 +56,7 @@ export default function DayForecastCard(props: ForecastProps) {
           />
           <div className="text-[#30373E] font-bold mx-auto">{props.temp}°C</div>
         </div>
-        <div className="basis-2/3 xl:basis-5/6 flex flex-col">
+        <div className="basis-2/3 md:basis-3/4 xl:basis-5/6 flex flex-col">
           <div className="text-[#30373E] text-md font-bold">{props.condition}</div>
           <div className="text-[#30373E] text-sm">{day}, {date} {month} {year}</div>
         </div>
