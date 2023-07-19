0023ceb18c2a2b747b09c1affba4b4b626cfac74
recta-rrae <raihanf787@gmail.com>
2023-07-19 23:02:38 +0700

    (API) : adding a default city to fetch weather data (default: Tokyo)

--- app/api/getCurrentWeatherSWR.ts
+++ app/api/getCurrentWeatherSWR.ts
@@ -7,8 +7,9 @@ import { getCityDateTimeSWR } from "./getCityDateTimeSWR";
 const fetcher = (url: string) => axios.get(url).then((res: { data: any; }) => res.data);
 
 export function getCurrentWeatherSWR(city: string) {
+
   const { data, error, isLoading, isValidating } = useSWR(
-    `https://api.weatherapi.com/v1/forecast.json?key=c9e6cb3718574d2293d42758230207&q=${city}&days=5&aqi=no&alerts=no`,
+    `https://api.weatherapi.com/v1/forecast.json?key=c9e6cb3718574d2293d42758230207&q=${city=="0,0"?"Tokyo":city}&days=5&aqi=no&alerts=no`,
     fetcher,
     { refreshInterval: 600000 }
   );
