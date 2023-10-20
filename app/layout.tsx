c8c95e9e5166f382ead8bd84668006f20d3c0886
Han <59479453+recta-rrae@users.noreply.github.com>
2023-10-20 14:48:05 +0700

    refactor : optimizing preline-ui useEffect for metadata in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,23 @@
-"use client";
 import "./globals.css";
 import { Inter } from "next/font/google";
-import { useEffect } from "react";
-import Head from "next/head";
+import type { Metadata } from "next";
 
 import CityContextProvider from "@/context/CityContextProvider";
 
 const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Tenki",
+  description: "Weather App",
+};
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  useEffect(() => {
-    import("preline" as any);
-  }, []);
   return (
     <html lang="en">
-      <Head>
-        <title>Tenki Weather App</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
       <body className={inter.className}>
         <div className="mx-10 lg:px-5 lg:mx-auto lg:container">
           <CityContextProvider>{children}</CityContextProvider>
