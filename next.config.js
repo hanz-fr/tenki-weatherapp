885199991e47656ce6a85f0ef393e5ee411922b0
Han <59479453+recta-rrae@users.noreply.github.com>
2023-10-21 00:36:54 +0700

    refactor : next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: {
-    OPEN_WEATHER_KEY: process.env.OPEN_WEATHER_KEY,
-  },
   images: {
     remotePatterns: [
       {
@@ -15,42 +12,18 @@ const nextConfig = {
       },
     ],
   },
-  async headers() {
+  async rewrites () {
     return [
       {
-        source: "/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,DELETE,PATCH,POST,PUT",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-          },
-        ],
-      },
-    ];
-  },
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: "http://api.weatherapi.com/:path*",
+        source: '/api/:path*',
+        destination: 'http://api.weatherapi.com/:path*',
       },
       {
-        source: "/api/:path*",
-        destination: "https://api.timezonedb.com/:path*",
-      },
-      {
-        source: "/api/:path*",
-        destination: "https://api.openweathermap.org/:path*",
-      },
-    ];
-  },
+        source: '/api/:path*',
+        destination: 'https://api.timezonedb.com/:path*',
+      }
+    ]
+  }
 };
 
 module.exports = nextConfig;
